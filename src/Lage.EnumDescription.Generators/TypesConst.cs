c27ee4fe170202c7ebcfac589cffefa8bedd458a
Madao <kaixuanniu@foxmail.com>
2026-05-21 14:03:41 +0800

    feat：添加常量类型枚举

---
+++ src/Lage.EnumDescription.Generators/TypesConst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lage.EnumDescription.Generators;
+
+internal class TypesConst
+{
+    public const string NotNullWhenTrue = "[global::System.Diagnostics.CodeAnalysis.NotNullWhen(true)]";
+
+    public const string Dictionary = "global::System.Collections.Generic.Dictionary";
+
+    public const string Lazy = "global::System.Lazy";
+}
