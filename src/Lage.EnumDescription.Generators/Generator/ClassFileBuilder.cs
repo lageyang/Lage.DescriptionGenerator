b52d0b4093f75367150b6148a8a13fc85a8be61e
madao <kaixuanniu@foxmail.com>
2026-06-08 10:43:24 +0800

    refactor：重构枚举扩展代码生成逻辑，提升可维护性
    重构生成器，将枚举扩展代码生成逻辑从 DescriptionGenerator 拆分至新建的 EnumFileBuilder 类，采用链式调用方式组织生成流程。新增 ClassFileBuilder 预留类扩展生成能力。优化了代码结构、缩进和注释，修复类型判断等细节，提升了可读性和正确性。

---
+++ src/Lage.EnumDescription.Generators/Generator/ClassFileBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Lage.EnumDescription.Generators.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lage.EnumDescription.Generators.Generator
+{
+    internal class ClassFileBuilder
+    {
+        private readonly StringBuilder sb;
+        private readonly TargetInfo info;
+        private readonly string fullName;
+        private int indent = 1;
+
+        public ClassFileBuilder(TargetInfo info)
+        {
+            this.info = info;
+            this.fullName = info.GetFullName();
+            sb = new StringBuilder(512);
+        }
+    }
+}
